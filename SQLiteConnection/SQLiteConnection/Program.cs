cdb710eeaaa7d461e2ee6152ba0744b8382425f3
silenroc1 <cornelisbouter@gmail.com>
2015-05-08 10:51:57 +0200

    Inlezen autompg.sql

--- SQLiteConnection/SQLiteConnection/Program.cs
+++ SQLiteConnection/SQLiteConnection/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
+using System.IO;
 
 namespace SQLite
 {
@@ -12,6 +13,37 @@ namespace SQLite
 
         static void Main(string[] args)
         {
+
+
+            SQLiteConnection m_dbConnection;
+            SQLiteConnection.CreateFile("autompg.sqlite");
+            m_dbConnection = new SQLiteConnection("Data Source=autompg.sqlite;Version=3;");
+            m_dbConnection.Open();
+
+            string s;
+            StreamReader str = new StreamReader("autompg.sql");
+            s = str.ReadToEnd();
+            SQLiteCommand command = new SQLiteCommand(s, m_dbConnection);
+            try
+            {
+                command.ExecuteNonQuery();
+
+                command = new SQLiteCommand("select * from autompg order by cylinders desc", m_dbConnection);
+
+                SQLiteDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                    Console.WriteLine("Brand: " + reader["brand"] + "\tCylinder: " + reader["cylinders"]); 
+
+            }
+
+            finally
+            {
+                m_dbConnection.Close();
+            }
+
+
+
+            /*
             SQLiteConnection m_dbConnection;
             SQLiteConnection.CreateFile("MyDatabase.sqlite");
             m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
@@ -42,7 +74,7 @@ namespace SQLite
             SQLiteDataReader reader2 = command4.ExecuteReader();
             while (reader2.Read())
                 Console.WriteLine("Name: " + reader2["name"] + "\tScore: " + reader2["score"]);
-
+            */
             Console.Read();
         }
     }
