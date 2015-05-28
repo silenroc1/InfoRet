c63a51296d8a6860af223844ea7837c639cc80d1
AKlinkmaster <kangoeroealex@hotmail.com>
2015-05-28 10:04:01 +0200

    small addition to ITA, now all commented for implementation

--- SQLiteConnection/SQLiteConnection/QueryProcessor.cs
+++ SQLiteConnection/SQLiteConnection/QueryProcessor.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
+using System.Collections.Specialized.OrderedDictionary;
 
 namespace InformationRetrieval
 {
@@ -11,19 +12,34 @@ namespace InformationRetrieval
     {
         private static SQLiteConnection m_dbConnection = Preprocessor.m_dbConnection;
 
-        private static void ITA(string[] query, int K) {
+
+        private static SortedDictionary<int, SQLiteDataReader> ITA(string[] query, int K) {
             HashSet<int> seen = new HashSet<int>();
-            Dictionary<int, SQLiteDataReader> buffer = new Dictionary<int, SQLiteDataReader>(K);
+            SortedDictionary<Int32, SQLiteDataReader> buffer = new SortedDictionary<Int32, SQLiteDataReader>();
             int p = 0; //= amount of matches in the database with the query
 
-            while(true){//get next Lk
+            SQLiteCommand command = new SQLiteCommand("select * from autompg ORDER BY ", m_dbConnection);
+
+            while(true){//there is a next Lk
                 for (int k = 1; k < p;k++ ) {
                     //Lk is the the location in the ordering that is given for the query
                     int TIDk = IndexLookupGetNextTID(9);
                     SQLiteDataReader Tk = TupleLookup(TIDk);
 
-                    if(stoppingCondition(Tk)){
-                        return;
+                    int score = 0;//score function
+                    if (buffer.Count < K) {
+                        buffer.Add(score, Tk);
+                    }
+                    else if(score > buffer.Keys.Last()) {
+                        buffer.Add(score, Tk);
+                        if (buffer.Count > K) {
+                            buffer.Remove(buffer.Keys.Last());
+                        }
+                    }
+
+
+                    if(stoppingCondition(Tk, buffer.Keys.Last())){
+                        return buffer;
                     }
                 }
             }
@@ -53,8 +69,8 @@ namespace InformationRetrieval
             return reader;
         }
 
-        private static bool stoppingCondition(SQLiteDataReader Tk) {
-            //Compute the highest possible score from what need to check
+        private static bool stoppingCondition(SQLiteDataReader Tk, int lowestScore) {
+            //Compute the highest possible score from what needs to be check
             //and compare that to the lowest score in the top-K
             
             return true;
@@ -62,6 +78,7 @@ namespace InformationRetrieval
 
         private static int IndexLookupGetNextTID(int Lk){
             //gegeven een orderning op de database (op een kolom), pak de ID van de volgende entry
+
             return 0;
         }
     }
